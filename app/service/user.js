82a5a8e15d7b43793a8d75fb4c2987217d9ad284
zzes <kuanglong0312@sina.com>
2018-04-26 17:27:54 +0800

    delete

--- app/service/user.js
+++ app/service/user.js
@@ -6,10 +6,19 @@ class UserService extends Service {
   async say() {
     return 'hello man'
   }
+  /**
+   * 将socketid和用户名一起存入redis
+   * @param {*} uid 
+   * @param {*} username 
+   */
   async setUser(uid,username) {
     const {ctx,app} = this
     await app.redis.set(uid, username)
   }
+  /**
+   * 通过socketid获取用户名
+   * @param {*} uid 
+   */
   async getUser(uid) {
     const {ctx,app} = this
     return await app.redis.get(uid)
