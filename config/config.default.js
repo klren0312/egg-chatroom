fd03be057ef0126e09b586a62e4514455b4788b0
zzes <kuanglong0312@sina.com>
2018-04-26 16:41:05 +0800

    配置文件

---
+++ config/config.default.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1524707769885_6419';
+
+  // add your config here
+  config.middleware = [];
+  // redis config
+  config.redis = {
+    client: {
+      port: 6379,
+      host: '127.0.0.1',
+      password: '',
+      db: 0
+    }
+  }
+  // socket.io config
+  config.io = {
+    init: {
+      wsEngine: 'uws'
+    },
+    namespace: {
+      '/': {
+        connectionMiddleware: ['connection'],
+        packetMiddleware: ['filter'],
+      }
+    },
+    redis: {
+      host:'127.0.0.1',
+      port: 6379,
+    }
+  }
+  return config;
+};
