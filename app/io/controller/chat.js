133734808df712b7f8efd536863266d053e8afc5
kuanglong0312@sina.com <klren@renzhizhongdeMacBook-Air.local>
2018-04-29 14:19:13 +0800

    update

--- app/io/controller/chat.js
+++ app/io/controller/chat.js
@@ -8,19 +8,18 @@ class NspController extends Controller {
     const message = ctx.args[0] || {}
     const roomId = ctx.socket.handshake.query.room
     let username = ctx.socket.handshake.query.username
-    const date = new Date().toLocaleString()
-    let data = {
-      user: username,
-      data: message,
-      date: date
-    }
-    // 将新数据推到redis, EX参数用来设置销毁时间
-    app.redis.lpush(roomId, JSON.stringify(data))
     // 将新数据推送给房间的所有人
     app.io.to(roomId).emit('new message', {
       data: message,
       user: username
     })
+    // 将新数据推到redis
+    let data = {
+      user: username,
+      data: message,
+      date: new Date().toLocaleString()
+    }
+    app.redis.lpush(roomId, JSON.stringify(data))
   }
 }
 
--- app/io/middleware/connection.js
+++ app/io/middleware/connection.js
@@ -3,29 +3,20 @@ let usernum = 0
 module.exports = app => {
   return async (ctx, next) => {
     // 建立连接
-    // 拿到房间号
+    // 拿到房间号 用户名
     let roomId = ctx.socket.handshake.query.room
     let username = ctx.socket.handshake.query.username
-    let uid = ctx.socket.id
+    // 加入房间
+    ctx.socket.join(roomId, () => {
+      app.io.to(roomId).emit('online',`欢迎新人加入:${username}`)
+    })
     // 获取历史数据
     let oldData = await app.redis.lrange(roomId, 0, 10)
     if (oldData) {
       ctx.socket.emit('old message', oldData)
     }
-    ctx.socket.join(roomId, () => {
-      app.io.to(roomId).emit('online',`欢迎新人加入:${username}`)
-    })
-    ctx.socket.emit('res', 'connected!')
-    ++usernum
-    app.io.emit('all user', {
-      usernum: usernum
-    })
     // 断开连接
     await next()
-    --usernum
-    app.io.emit('all user', {
-      usernum: usernum
-    })
     ctx.socket.leave(roomId,() => {
       app.io.to(roomId).emit('online',`欢迎新人退出:${username}`)
     })
