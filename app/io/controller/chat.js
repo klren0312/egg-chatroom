4b4dbd89dfbd6355f1bc5b13d9583f8e81b50433
zzes <kuanglong0312@sina.com>
2018-04-29 17:45:53 +0800

    time

--- app/io/controller/chat.js
+++ app/io/controller/chat.js
@@ -5,14 +5,18 @@ const Controller = require('egg').Controller;
 class ChatController extends Controller {
   async message() {
     const {ctx, app} = this
+    
     const message = ctx.args[0] || {}
     const roomId = ctx.socket.handshake.query.room
     let username = ctx.socket.handshake.query.username
+    // console.log(new Date().getTime() -  message)
+    
     // 将新数据推送给房间的所有人
     app.io.to(roomId).emit('new message', {
       data: message,
       user: username
     })
+    
     // 将新数据推到redis
     let data = {
       user: username,
@@ -20,6 +24,7 @@ class ChatController extends Controller {
       date: new Date().toLocaleString()
     }
     app.redis.lpush(roomId, JSON.stringify(data))
+    
   }
 }
 
